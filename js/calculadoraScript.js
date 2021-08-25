8beedb4f3c0f6e4b9b97553186f2a9689a8804fb
ArdisonRubim <ardison.rubim@gmail.com>
2021-08-24 23:10:43 -0300

    Logica concluida e projeto finalizado

--- js/calculadoraScript.js
+++ js/calculadoraScript.js
@@ -1,5 +1,34 @@
 // Função para manipular os dados
+function calculo(tipo, valor) {
 
-function calculo(){
-    
+    if(tipo === 'acao') {
+
+        if(valor === 'c') {
+            // Limpar o visor da calculadora
+            document.getElementById('visor').value = ''
+        }
+
+        // Tratando as operações
+        if(valor === '/' || valor === '-' || valor === '*' || valor === '+' || valor === '.' ) {
+            document.getElementById('visor').value += valor
+        }
+
+        // Calculando resultado
+        if(valor === '=') {
+            // Recupera e faz a operacão do valor
+            var valor_visor = eval(document.getElementById('visor').value)
+            // Exibir resultado
+            document.getElementById('visor').value = valor_visor
+        }
+
+        // Raiz quadrada
+        if(valor === '^2'){
+            valor_recebido = document.getElementById('visor').value
+            valor_raiz = (valor_recebido * valor_recebido)
+            document.getElementById('visor').value = valor_raiz
+        }
+    } else if (tipo === 'valor') {
+        //Mostrar o valor no visor
+        document.getElementById('visor').value += valor
+    }
 }
